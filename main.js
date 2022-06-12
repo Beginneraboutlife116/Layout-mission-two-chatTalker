f5e8e74c9c30bae9ba1dab209d583e4fca69f839
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-06-12 17:48:51 +0800

    first finish index.html

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
-// click 'TOP' button
+const outerScroller = document.querySelector('.horizontal-scroller')
+const innerScroller = document.querySelector('.horizontal-scroller__list')
+const scrolledElements = document.querySelectorAll('[data-select]')
+const scrollers = document.querySelectorAll('[data-scroller]')
+
+if (window.innerWidth >= 1104) {
+  scrollers[0].dataset.scroller = true
+  scrollers[1].dataset.scroller = true
+} else {
+  scrollers[0].dataset.scroller = false
+  scrollers[1].dataset.scroller = true
+}
+
+window.addEventListener('resize', (event) => {
+  //! window resize => just for work
+  const currentWidth = event.target.innerWidth
+  if (currentWidth >= 1104) {
+    innerScroller.dataset.scroller = false
+    outerScroller.dataset.scroller = true
+  } else {
+    innerScroller.dataset.scroller = true
+    outerScroller.dataset.scroller = false
+  }
+})
+
+//* click 'TOP' button
 const backToTop = document.querySelector('[data-back-to-top]')
 backToTop.addEventListener('click', () => window.scrollTo(top))
 
-// scroll mechanism
-const scroller = document.querySelector('[data-scroller]')
-const scrolledElements = document.querySelectorAll('[data-select]')
-
-const scrollUnit = scroller.scrollWidth / (scrolledElements.length * 2)
+//* scroll mechanism
 let currentScrollNumber = 0
-let currentScrollX = scroller.scrollLeft
 
-scroller.addEventListener('scroll', () => {
-  currentScrollX = scroller.scrollLeft
-  scrolledElements[currentScrollNumber].dataset.select = false
-  if (currentScrollX <= scrollUnit) {
-    currentScrollNumber = 0
-  } else if (currentScrollX <= scrollUnit * 3) {
-    currentScrollNumber = 1
-  } else if (currentScrollX <= scrollUnit * 5) {
-    currentScrollNumber = 2
-  } else {
-    currentScrollNumber = 3
+scrollers.forEach(scroller => {
+  if (scroller.dataset.scroller) {
+    scroller.addEventListener('scroll', () => {
+      let currentScrollX = scroller.scrollLeft
+      scrolledElements[currentScrollNumber].dataset.select = false
+      if (currentScrollX === 0) {
+        currentScrollNumber = 0
+      }
+      if (currentScrollX === window.innerWidth) {
+        currentScrollNumber = 1
+      } 
+      if (currentScrollX === window.innerWidth * 2) {
+        currentScrollNumber = 2
+      } 
+      if (currentScrollX === window.innerWidth * 3) {
+        currentScrollNumber = 3
+      } 
+      scrolledElements[currentScrollNumber].dataset.select = true
+    })
   }
-  scrolledElements[currentScrollNumber].dataset.select = true
 })
